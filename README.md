e14a01281943bef8489d694316aa33a52448e576
Ruben Cancho <{ID}+{username}@users.noreply.github.com>
2022-12-15 21:57:17 +0100

    Añadidas instrucciones de instalación

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # RL_tutorial
+
+## Instalación
+
+pip install gym
+
+pip install git+https://github.com/carlosluis/stable-baselines3@fix_tests
--- cartpole/mlp_policy.py
+++ cartpole/mlp_policy.py
@@ -2,7 +2,7 @@ import gym
 
 from stable_baselines3 import A2C
 
-env = gym.make("CartPole-v1")
+env = gym.make("CartPole-v1", render_mode="rgb_array")
 
 model = A2C("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=15_000)
